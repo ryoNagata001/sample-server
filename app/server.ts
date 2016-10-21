8db6cf48b9358e30163c4b05d2ecacf7f9876dfb
BrianDGLS <briandgls92@gmail.com>
2016-10-21 15:25:45 +0100

    Initial commit

---
+++ app/server.ts
@@ -0,0 +1,12 @@
+import * as express from 'express';
+
+import {WelcomeController} from './controllers';
+
+const app: express.Application = express();
+const port: number = process.env.PORT || 3000;
+
+app.use('/welcome', WelcomeController);
+
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}/`);
+});
